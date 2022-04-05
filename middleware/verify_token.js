f9e4c61341847ee58ce916bdbe59b1c4df0995a4
Parthpatodi <patodiparth@gmail.com>
2022-04-05 14:58:34 +0530

    gtgy

--- middleware/verify_token.js
+++ middleware/verify_token.js
@@ -1,15 +1,16 @@
 const jwt = require('jsonwebtoken');
-exports.verifyToken = (request,response)=>{
+exports.verifyToken = (request,response,next)=>{
     try{
-        console.log("token "+ request.headers.Authorization);
-        if(!request.headers.Authorization)
-          return response.status(401).send("Unauthorized Request");
-        if(request.headers.Authorization)
-          return response.status(401).send("Unauthorized Request"); 
-        let token = request.headers.Authorization.split(" ")[1];
+        console.log("token "+ request.headers.authorization);
+        if(!request.headers.authorization)
+          return response.status(200).send("first if - Unauthorized Request");
+        if(request.headers.authorization == null)
+          return response.status(200).send("second if - Unauthorized Request"); 
+        let token = request.headers.authorization.split(" ")[1];
         let payload = jwt.verify(token,'parth');
         next();                 
     }catch(err){
-        return response.status(401).send("Unauthorized User")
+        console.log(err);
+        return response.status(200).send("Inside catch Unauthorized User")
      }
 }
